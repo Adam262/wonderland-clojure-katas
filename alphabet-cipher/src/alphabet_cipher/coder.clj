c6e1321073273eae2684e5149149a5fd34680cb6
Adam262 <abarcan@gmail.com>
2017-01-23 00:30:48 -0500

    WIP - working on decipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -35,6 +35,20 @@
     (vec (take-cycle encrypted-message keyword))
     (vec encrypted-message))))
 
-(defn decipher [cipher message]
-  "decypherme")
+(defn decipher-single-char [message-char encrypted-char] (
+  nth (
+    filter
+      (fn [key] (= (string/index-of (get grid key) encrypted-char) (idx message-char)))
+      (keys grid)) 0))
 
+(defn cycled-keyword [message encrypted-message] (
+  string/join (
+    map decipher-single-char (vec message) (vec encrypted-message))))
+
+(defn decipher [message encrypted-message]
+  (let [cycled (cycled-keyword message encrypted-message)]
+  (subs cycled 0 (nth 
+    (filter
+      (fn [x] (= cycled (string/join (take (count cycled) (cycle (subs cycled 0 x))))))
+      (range (count cycled))) 
+        0))))
